da8c1140a494f0933ad958ba5df03b4b528b903d
Natalena Groom <ng0745824@otc.edu>
2018-09-03 00:42:37 -0500

    Completed "1, 2, 3 Code" Coding Time Problems.

---
+++ app.js
@@ -0,0 +1,31 @@
+//Write a program that displays your name and age.
+let name = "Natalena Groom";
+let age = 22;
+console.log("Name: " + name);
+console.log("Age: " + age);
+
+//Write a program that displays the result of adding, subtracting, mulitplying and dividing 6 by 3.
+let num1 = 6;
+let num2 = 3;
+let result;
+result = num1 + num2;
+console.log(num1 + " + " + num2 + " = " + result);
+result = num1 - num2;
+console.log(num1 + " - " + num2 + " = " + result);
+result = num1 * num2;
+console.log(num1 + " * " + num2 + " = " + result);
+result = num1 / num2;
+console.log(num1 + " / " + num2 + " = " + result);
+
+/*
+    Observe the following program and try to predict the values it displays.
+
+        console.log(4 + 5);
+        console.log("4 + 5");
+        console.log("4" + "5");
+
+    Check your prediction by executing it.
+*/
+console.log(4 + 5); //prediction: number 9
+console.log("4 + 5"); //prediction: string 4 + 5
+console.log("4" + "5"); //prediction: string 45
